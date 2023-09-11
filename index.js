eccaa8b5de464b48515caab093b2fa804507f9bd
Diana <taaldiana@gmail.com>
2023-09-11 18:13:41 +0300

    view kaust

--- index.js
+++ index.js
@@ -1,35 +1,69 @@
-const express = require('express')
+// application packages
+const express = require ('express')
 const app = express()
 
 const path = require('path')
-const hbs = require('express-handlebars');
-app.set('views', path.join(_dirname, 'views'));
-app.set('view-engine', 'hbs');
+//add template engine
+const hbs = require("express-handlebars");
+//setup template engine directory and files extension
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
-    defaultLayout: 'main',
-    layoutsDir: __dirname+ '/views/layouts/',
+    defaultLayout:'main',
+    layoutsDir:__dirname+'/views/layouts',
 }))
+//setup static public directory
 app.use(express.static('public'));
 
-const mysql = require('mysql')
+const mysql = require ('mysql')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
-
-let con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwerty",
-    database: "mysql"
+// create database connection
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"qwerty",
+    database: "joga_mysql"
 })
 
 con.connect(function(err) {
-    if (err) throw (err);
-    console.log("Connected to mysql db");
+    if (err) throw err;
+    console.log("connected to joga_mysql db");
 })
 
+//show all articles - index page
+app.get('/',(req,res)=> {
+    let query = "SELECT*FROM article";
+    let articles = []
+    con.query(query,(err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index',{
+            articles:articles
+        })
+    })
+});
+
+//show article by this slug
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * from article WHERE slug="${req.params.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if(err) throw err;
+        article = result
+        console.log(article)
+        res.render('article', {
+            article: article
+        })
+    });
+});
+
+
+
+//app start point
 app.listen(3000, () => {
-    console.log('App is started at localhost:3000');
+    console.log('App is started at http://localhost:3000');
 })
